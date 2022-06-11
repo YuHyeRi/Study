b25c1aa315363b9d77c13ad1194885df37bb38b1
YuHyeRi <tgmary09@gmail.com>
2022-06-11 22:21:34 +0900

    0611 flex와 position

--- vscode/script.js
+++ vscode/script.js
@@ -1,14 +1,14 @@
 $(function() {
-    alert('test'); // 테스트용
+    // alert('test'); // 테스트용
 
     var body = '#wrap > .container > div > div';
 
     $(body).mouseenter(function () { 
         console.log('open');    // 테스트용
-        $(this).find('.img').show();
+        $(this).find('.imgText').show();
     });
     $(body).mouseleave(function () {
         console.log('close');   // 테스트용
-        $(this).find('.img').hide();
+        $(this).find('.imgText').hide();
     });
 })
